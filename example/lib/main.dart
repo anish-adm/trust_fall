38170e7990cdf658dc61a3030140c3e02ade952a
Anish M <anish_adm@yahoo.com>
2019-03-24 15:08:29 -0400

    removed unnecessary dependencies

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:trust_fall/trust_fall.dart';
 
 void main() => runApp(MyApp());
